46ea95b25864345a99198e0970ccb4ff259cbde8
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-22 13:20:37 -0500

    Add notes for indexing and len function

--- strings.py
+++ strings.py
@@ -40,7 +40,20 @@ name_and_age = name + " " + age # this will raise a TypeError because age is not
 
 name_and_age = name + " " + str(age) # this will work because we converted age to a string
 
-# f-string is a better way to concatenate strings
+
+# Augmented assignment operator for concatenation
+# += perform concatenation and assignment
+
+name = "John"
+last_name = "Doe"
+
+full_name = name
+full_name += last_name
+print(full_name)
+
+# f-string means formatted strings
+
+# f-string is a better way to concatenate strings also used for Interpolation
 greeting = f"Hello, {name}! You are {age} years old."
 print(greeting)
 
@@ -60,3 +73,21 @@ print(full_name)
 # string concatenation using format() method
 full_name = "{} {}".format(first_name, last_name)
 print(full_name)
+
+
+# get a length of string and work with particular character from string , process called 'indexing'
+my_str = "Hello world"
+print(len(my_str))
+
+# Each character has position called 'index' start with 0, you can access the character from string using '[]' square bracket
+
+my_str = "Hello world"
+print(my_str[0])
+print(my_str[6])
+
+# with negative indexing you can get character from end
+
+print(my_str[-1]) # print last character from the string 'd'
+print(my_str[-2]) # print second last character from the string 'l'
+
+# if you -12 or 12 you will get the IndexError
