4e8ed02f091c4529febaf32e5d737d9a3955c91d
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-27 12:44:15 -0500

    Add notes about code block and indentation

--- condition_logical.py
+++ condition_logical.py
@@ -5,5 +5,11 @@
 # >= greater than or equal
 # <= less than or equal
 
+# === gives you syntax error (Invalid syntax)
 
 # In python , code block is determined by Indentation 
+
+# code block - group of statements that belong together
+
+# if condition:
+#     pass 
