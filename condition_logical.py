419ae8a37607a6ce4ae9dd9cc1d7bdb6eb49da9c
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-27 16:37:20 -0500

    Add note for and operator

--- condition_logical.py
+++ condition_logical.py
@@ -8,8 +8,42 @@
 # === gives you syntax error (Invalid syntax)
 
 # In python , code block is determined by Indentation 
+# 4 spaces recommended for Indentation
 
 # code block - group of statements that belong together
 
 # if condition:
 #     pass 
+# elif condition: (else if)
+#     pass  
+# else:
+#     pass (if condition not true it will execute this statement/ code block)
+
+
+# Truthy and Falsy values
+
+# Falsy value
+#------------#
+# None
+# False
+# '' (empty string)
+# Integer 0
+# Float 0.0
+
+# Truthy value
+#--------------#
+# non-zero and non-empty string are truthy
+
+# bool() function to check value is truthy or falsy , returns True or False
+
+
+# Three Boolean operator ( and, or and not)
+
+# "and" known as 'short-circuit operator' - means python checks from left to right and stops as it determines the final result
+
+
+# can take two operands ( thats why it is called binary operator)
+# There’s no limit to the number of and operators you can use when you’re building a compound expression.
+# if first falsy return first
+# if first truthy return second
+
