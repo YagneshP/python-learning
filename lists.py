525e019a6111aea1027a1dba655bff8cb6545443
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-30 20:02:39 -0500

    Add note for del and in keyword

--- lists.py
+++ lists.py
@@ -34,3 +34,18 @@ print(len(list(element_0))) # should print 10 , here we convert first element wh
 skills[1] = 'nextJs' # here we are changing second element value from 'react' to 'nextJs'
 
 print(skills) # should print ['javascript', 'nextJs', 'python', ['css', 'scss', 1]]
+
+# to remove any element from the list you can use 'del' keyword and reference the index number from where you want to "delete the element"
+
+developer = ['Jane Doe', 23, 'Python Developer']
+del developer[1]
+print(developer) # ['Jane Doe', 'Python Developer']
+
+
+# "in" keyword to check of element inside the list , return True or False
+
+react_skill = "react" in skills
+next_js_skill = "nextJs" in skills
+
+print('react skill', react_skill) # should print react skill False
+print('nextJs skill', next_js_skill) # should print nextJs skill True
