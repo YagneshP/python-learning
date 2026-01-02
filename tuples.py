cf6ddb58faa79e2142001f97f7f5abe3acc74e23
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-01 22:37:31 -0500

    Add note for the tuple immutability

---
+++ tuples.py
@@ -0,0 +1,14 @@
+#### ============ Tuple =============== ###
+
+# - same like list but tuple are "immutable" , can not change it
+
+# - ordered sequence of values
+# - can contain set of mixed data type
+
+developer = ('Alice', 23, 'Python Developer')
+
+# - TypeError if you update the tuple
+
+print("----- you will get TypeError if you try to update tuple by changing value of an element ------")
+# commented code below give the TypeError for assignment
+# developer[0] = "Jane"
