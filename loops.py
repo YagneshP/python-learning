52ce8fe523941bd34210d7dcd63993f36e315448
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-03 08:15:22 -0500

    Add note for the for loop

---
+++ loops.py
@@ -0,0 +1,16 @@
+### Loops are used to repeat code block for set of number of times
+
+### ==== for loop ====
+
+# - go through each element of the iterable object
+
+programming_languages = ["Java", "Rust", "Go", "Python"]
+
+# for ... in ... :
+#   code block to repeat # -- should be indented otherwise will get IndentationError
+
+for lang in programming_languages:
+  print(lang)
+
+# you can use it for string as well
+# you can nest them
