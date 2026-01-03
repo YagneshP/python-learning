5ebf80b71fab7c1a924c8a1879195a73c972982d
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-03 08:31:09 -0500

    Add note for while loop with guess the number game

--- loops.py
+++ loops.py
@@ -4,6 +4,8 @@
 
 # - go through each element of the iterable object
 
+
+
 programming_languages = ["Java", "Rust", "Go", "Python"]
 
 # for ... in ... :
@@ -14,3 +16,33 @@ for lang in programming_languages:
 
 # you can use it for string as well
 # you can nest them
+
+for char in 'code':
+  print(char)
+
+
+fruits = ["Mango", 'orange', 'Apple', 'Banana']
+category = ['Fruit', 'Vegetable']
+
+for cate in category:
+  for fruit in fruits:
+    print(cate, fruit)
+
+
+#### while loop - it repeat the code block until condition is False
+
+# while ...condition..:
+#   code block
+
+
+# Guess the number
+
+secret_number = 7
+guessed_number = 0
+
+while secret_number != guessed_number:
+  guessed_number = int(input("Guess the number between 1 to 9: "))
+  if(guessed_number != secret_number) :
+    print("ohh! you have missed, Try Again")
+  
+print(' Saat crore')
