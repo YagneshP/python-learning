b189b439beb65cacb5549c31ecc26a3a09bdd44c
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-19 18:51:32 -0500

    Add update_setting function

--- certification_projects/configuration_manager.py
+++ certification_projects/configuration_manager.py
@@ -1,10 +1,23 @@
 def add_settings(settings, tuple):
   key, value = tuple
   key = key.lower()
+  value = value.lower()
   if key in settings:
     msg = f"Setting '{key}' already exists! Cannot add a new setting with this name."
     return msg
   else:
     settings[key] = value
     msg = f"Setting '{key}' added successfully with value '{value}'."
+    return msg
+
+def update_settings(settings, tuple):
+  key, value = tuple
+  key = key.lower()
+  value = value.lower()
+  if key in settings:
+    settings[key] = value
+    msg = f"Setting '{key}' updated to '{value}' successfully!"
+    return msg
+  else:
+    msg = f"Setting '{key}' does not exist! Cannot update a non-existing setting."
     return msg
