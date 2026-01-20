4573023280b59546d3343c4c6ba85ee8b9bdef42
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-20 05:18:54 -0500

    Use of try and except block for error handling

--- errors_in_python.py
+++ errors_in_python.py
@@ -2,7 +2,10 @@ import pdb
 
 # pdb is a debugger for python
 def divide(a, b):
-  pdb.set_trace() # this will stop the execution of the program and open the pdb debugger
+  # pdb.set_trace() # this will stop the execution of the program and open the pdb debugger
+  # IDE debugger will also stop the execution of the program and open the debugger
+  # Select the line of code you want to debug and click on the debugger icon
+  # You can also set a breakpoint by clicking on the line of code and pressing F9
   return a / b
 
 print(divide(10, 2))
