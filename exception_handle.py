75f8d76f1e0c78e42fd80311bf7562143f485751
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-20 05:21:31 -0500

    Multiple except blocks

--- exception_handle.py
+++ exception_handle.py
@@ -6,3 +6,18 @@ else:
   print("No error occurred")
 finally:
   print("This will always execute")
+
+# multiple exceptions
+
+try:
+  x = int('abc')
+  result = 10 / x
+except ValueError:
+  print("Error: Invalid value")
+except ZeroDivisionError:
+  print("Error: Division by zero")
+else:
+  print("No error occurred")
+finally:
+  print("This will always execute")
+  
