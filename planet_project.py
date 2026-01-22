4c139051cf7cc0f6353abf663e32395c2a9af441
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-22 13:16:04 -0500

    Add str method for the class

--- planet_project.py
+++ planet_project.py
@@ -1,3 +1,4 @@
+
 class Planet:
   def __init__(self, name, planet_type, star):
     if not isinstance(name, str) and not isinstance(planet_type, str) and not isinstance(star, str):
@@ -8,3 +9,9 @@ class Planet:
     self.name = name
     self.planet_type = planet_type
     self.star = star
+
+  def orbit(self):
+    return (f"{self.name} is orbiting around {self.star}...")
+  
+  def __str__(self):
+    return(f"Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}")
