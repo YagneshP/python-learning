06e634452400ae6643d189942af31b73072bce65
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-27 01:14:47 -0500

    Add withdraw method starter code

---
+++ certification_projects/buget_app.py
@@ -0,0 +1,12 @@
+class Category:
+  def __init__(self, name):
+    self.name = name
+    self.ledger = []
+
+  def deposit(self, amount, description=''):
+    self.ledger.append({'amount': amount, 'description': description })
+
+  def withdraw(self, amount, description=''):
+    self.ledger.append({'amount' : -amount, 'description': description})
+  
+  
