9b6c616ab8213b6755e5644ced1b4c343adb203e
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-01-27 01:28:47 -0500

    Add check funds method

--- certification_projects/buget_app.py
+++ certification_projects/buget_app.py
@@ -9,4 +9,19 @@ class Category:
   def withdraw(self, amount, description=''):
     self.ledger.append({'amount' : -amount, 'description': description})
   
-  
+  def transfer(self, amount, destination):
+    if self.withdraw(amount, f'Transfer to {destination.name}'):
+      return True
+    if self.deposit(self, amount, f'Transfer from {self.name}'):
+      return True
+    return False
+
+  def check_funds(self, amount):
+    balance = 0
+    # iterate over self.ledger = {amount, description}
+    for amt , desc in self.ledger:
+      balance += amt
+    if balance < amount or balance == 0:
+      return False
+    else :
+      return True
