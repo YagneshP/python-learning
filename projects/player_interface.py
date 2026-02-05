dcc0a60ca3da1c9aee13aaa6eb60f54657245eea
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-02-05 16:44:36 -0500

    Add logic to make move method

--- projects/player_interface.py
+++ projects/player_interface.py
@@ -9,3 +9,6 @@ class Player(ABC):
   
   def make_move(self):
     random_move = random.choice(self.moves)
+    self.position = tuple(random_move)
+    self.path.append(self.position)
+    return self.position
