c09124f955cdc8dd10717693a879f0d3ea8799a6
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-02-06 12:39:11 -0500

    Add method to print rectangle

--- certification_projects/polygon_area_calculator.py
+++ certification_projects/polygon_area_calculator.py
@@ -19,3 +19,16 @@ class Rectangle:
   
   def get_diagonal(self):
     return math.sqrt(self._width ** 2 + self._height ** 2)
+  
+  def get_picture(self):
+    result = ''
+    if self._width > 50 or self._height > 50:
+      result += 'Too big for picture.'
+      return result
+    for h in range(0, self._height):
+      result += f"{'*' * self._width}\n"
+    return result
+  
+rect_1 = Rectangle(5, 10)
+
+print(rect_1.get_picture())
