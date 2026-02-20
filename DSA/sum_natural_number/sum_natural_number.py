9a4ec5c1592e5aab06650503aa3fc768b44abb4c
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-02-20 01:05:06 -0500

    Add loop aproach for sum natural number

--- DSA/sum_natural_number/sum_natural_number.py
+++ DSA/sum_natural_number/sum_natural_number.py
@@ -7,8 +7,17 @@ def sum_natural_number_recur(n):
     return 1
   return n + sum_natural_number_recur(n-1)
 
+def loop_sum_natural_number(n):
+  sum = 0
+  i = 1
+  for i in range(i, n+1):
+    sum += i
+  return sum
+
 if __name__ == "__main__":
   n = 5
   print(sum_of_natural_number(n))
   m = 6
   print(sum_natural_number_recur(m))
+  c = 4
+  print(loop_sum_natural_number(c))
