0a117a84fff88ebe8c9be964c6c776988e5b0939
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-02-25 14:49:09 -0500

    Add comment for understanding

--- DSA/Recursion/print_n_to_1.py
+++ DSA/Recursion/print_n_to_1.py
@@ -1,8 +1,8 @@
 def printNumber(n):
-  if n <= 0:
+  if n <= 0: #base case (it handle the negative number also)
     return
   print(n)
-  printNumber(n-1)
+  printNumber(n-1) # recursive case
 
 printNumber(5)
   
