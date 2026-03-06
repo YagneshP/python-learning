a2d9707a3f60ac49f22a0990f3e10ee6632e560d
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-03-06 15:51:02 -0500

    Add a comment for print function

--- DSA/Recursion/print_n_to_1.py
+++ DSA/Recursion/print_n_to_1.py
@@ -1,7 +1,10 @@
 def printNumber(n):
-  if n <= 0: #base case (it handle the negative number also)
+  if n == 0: #base case (it handle the negative number also)
     return
   print(n)
+  # print(n, end=' ') 
+  # print(n) — prints the number
+  # end=' ' — specifies what to print at the end (replaces the default \n newline)
   printNumber(n-1) # recursive case
 
 printNumber(5)
